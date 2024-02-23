fcd05c1b4a69600bded186198f6b194b1378da2b
awaas <alexandra.waas@hof-university.de>
2024-02-23 13:40:36 +0100

    Added imprint github url

--- app/src/main/java/com/example/friendlynotes/ImprintActivity.kt
+++ app/src/main/java/com/example/friendlynotes/ImprintActivity.kt
@@ -1,11 +1,24 @@
 package com.example.friendlynotes
 
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ImprintActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imprint)
+
+        val buttonDeveloper: Button = findViewById(R.id.buttonDeveloper)
+
+        val intent= Intent(this, MainActivity::class.java)
+
+        buttonDeveloper.setOnClickListener {
+            val implicitIntentDeveloper = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/alexandrawaas"))
+            startActivity(implicitIntentDeveloper)
+        }
     }
 }
